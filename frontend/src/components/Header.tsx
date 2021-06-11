4f9394cf78419a11c7c491a4e529a99c54903ab7
adam <paddenadam@gmail.com>
2021-06-11 09:47:16 -0400

    started header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+
+    return (
+        <div className="Header">
+            <h1>Quick Click</h1>
+        </div>
+    )
+}
+
+export default Header;
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Playlist, Data, Track } from "../model/Playlist";
 import { findPlaylist, getToken } from "../service/SpotifyApiService";
 // import ReactAudioPlayer from "react-audio-player";
-import "./Main.css";
+import "./Home.css";
 
 function Main() {
   const [playlist, setPlaylist] = useState<Playlist | null>(null);
@@ -35,7 +35,7 @@ function Main() {
 
   return (
     <div className="Main">
-      <h1>GAME</h1>
+      <h3>Home</h3>
       <p> {playlist?.data.name} </p>
       <p>  </p>
       {/* <ReactAudioPlayer src={playlist?.data.tracks.items[0].track.preview_url} controls /> */}
