735c1401dd5a9e55439910760c57e6482d0eb1b1
saniahsafdar <saniahsafdar@gmail.com>
2021-06-14 13:28:06 -0400

    Added Countdown Timer

--- frontend/src/components/AudioPlayer.tsx
+++ frontend/src/components/AudioPlayer.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { Playlist } from "../model/Playlist";
 import "./AudioPlayer.css";
+import CountDownTimer from "./CountDownTimer";
 
 interface Props {
     trackNumber: number;
@@ -8,8 +10,11 @@ interface Props {
 
 function AudioPlayer({ trackNumber, gamePlaylist }: Props) {
 
+
+
     return(
         <div className="AudioPlayer">
+            <CountDownTimer hours={0} minutes={0} seconds={10}/>
             <h2 className="title">{gamePlaylist?.data.tracks.items[trackNumber].track.name}</h2>
             <h3 className="artist">{gamePlaylist?.data.tracks.items[trackNumber].track.artists[0].name}</h3>
             <audio src={gamePlaylist?.data.tracks.items[trackNumber].track.preview_url} controls ></audio>
