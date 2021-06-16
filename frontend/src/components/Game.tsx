5768230897ce32a56ddc151f619ba1a30ca55f1e
saniahsafdar <saniahsafdar@gmail.com>
2021-06-16 14:59:21 -0400

    More CSS changes

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -107,13 +107,11 @@ function Game() {
 
   return (
     <div className="Game">
-      <div className="Home">
-        <NavLink to="/">Home</NavLink>
-      </div>
-      <button onClick={() => generateTrackIndex()}>Play Game</button>
 
       {gamePlaylist?.data.name}
-
+      { 
+      <button onClick={() => generateTrackIndex()}>Play Game</button>
+      }
       {trackNumber > -1 && playedCount <= 11 && (
         <div>
           <div className="audio-player">
