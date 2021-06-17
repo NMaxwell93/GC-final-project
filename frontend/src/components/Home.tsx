9510964425c87aa1b1f25324874f68f696febeab
Anneliese Sparks <sparksanneliese@gmail.com>
2021-06-17 16:39:45 -0400

    added more playlists and graphics

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -10,8 +10,11 @@ function Home() {
     <div className="Home">
       <div className="playlistButtons">
 
+      <div className="fifties">
+        <NavLink to="/game/37i9dQZF1DWSV3Tk4GO2fq"><button type="button">50s Playlist</button></NavLink>
+      </div>
       <div className="sixties">
-        <NavLink to="/game/37i9dQZF1DXaKIA8E7WcJj?si"><button type="button">60s Playlist</button></NavLink>
+        <NavLink to="/game/37i9dQZF1DXaKIA8E7WcJj"><button type="button">60s Playlist</button></NavLink>
       </div>
       <div className="seventies">
         <NavLink to="/game/37i9dQZF1DWTJ7xPn4vNaz"><button>70s Playlist</button></NavLink>
@@ -21,6 +24,15 @@ function Home() {
       </div>
       <div className="nineties">      
         <NavLink to="/game/37i9dQZF1DXbTxeAdrVG2l"><button>90s Playlist</button></NavLink>
+      </div>
+      <div className="twothousands">      
+        <NavLink to="/game/37i9dQZF1DX4o1oenSJRJd"><button>00s Playlist</button></NavLink>
+      </div>
+      <div className="twothousandten">      
+        <NavLink to="/game/37i9dQZF1DX5Ejj0EkURtP"><button>10s Playlist</button></NavLink>
+      </div>
+      <div className="twentytwenty">      
+        <NavLink to="/game/1UUCxq5cYwrh9tZNMujWlO"><button>20s Playlist</button></NavLink>
       </div>
       {/* <div className="aughts">
         <button><NavLink to="/game/1udqwx26htiKljZx4HwVxs">00's Playlist</NavLink></button>
